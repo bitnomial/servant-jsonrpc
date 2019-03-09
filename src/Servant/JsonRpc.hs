d4a7e84fff4c36128e4530edf543c4f42559dbcd
Ian Shipman <ics@gambolingpangolin.com>
2019-03-09 16:21:11 -0600

    draft of HasServer instance

--- src/Servant/JsonRpc.hs
+++ src/Servant/JsonRpc.hs
@@ -14,13 +14,16 @@
 module Servant.JsonRpc where
 
 
-import           Data.Aeson          (FromJSON (..), ToJSON (..), object,
-                                      withObject, (.:), (.:?), (.=))
+import           Control.Applicative (liftA3)
+import           Data.Aeson          (FromJSON (..), ToJSON (..), Value (Null),
+                                      object, withObject, (.:), (.:?), (.=))
+import           Data.Aeson.Types    (Parser)
 import           Data.Proxy
 import           Data.Word           (Word64)
 import           GHC.TypeLits        (KnownSymbol, Symbol, symbolVal)
 import           Servant.API         ((:>), JSON, Post, ReqBody)
 import           Servant.Client.Core (HasClient (..), RunClient)
+import           Servant.Server      (HasServer (..))
 
 
 data Request p
@@ -36,33 +39,62 @@ instance ToJSON p => ToJSON (Request p) where
                , "id" .= ix ]
 
 
+versionGuard :: String -> Parser a -> Parser a
+versionGuard v x
+    | v == "2.0" = x
+    | otherwise  = fail "unknown version"
+
+
+instance FromJSON p => FromJSON (Request p) where
+    parseJSON = withObject "JsonRpc Request" $ \obj -> do
+        ix <- obj .: "id"
+        method <- obj .: "method"
+        p <- obj .: "params"
+        version <- obj .: "jsonrpc"
+
+        versionGuard version . pure $ Request method p ix
+
+
 data Response e r
     = Result Word64 r
-    | Errors Int String (Maybe e)
+    | Errors (JsonRpcErr e)
+
+data JsonRpcErr e = JsonRpcErr Int String (Maybe e)
 
 instance (FromJSON e, FromJSON r) => FromJSON (Response e r) where
     parseJSON = withObject "Response" $ \obj -> do
         ix <- obj .: "id"
-        version :: String <- obj .: "jsonrpc"
+        version <- obj .: "jsonrpc"
         result <- obj .:? "result"
         err <- obj .:? "error"
         versionGuard version $ pack ix result err
 
         where
 
-        versionGuard v x
-            | v == "2.0" = x
-            | otherwise  = fail "unknown version"
-
         pack (Just ix) (Just r) Nothing = pure $ Result ix r
-        pack Nothing Nothing (Just e)   = parseErr e
+        pack Nothing Nothing (Just e)   = Errors <$> parseErr e
         pack _ _ _                      = fail "invalid response"
 
-        parseErr = withObject "Error" $ \err -> do
-            code <- err .: "code"
-            msg <- err .: "message"
-            dat <- err .:? "data"
-            return $ Errors code msg dat
+        parseErr = withObject "Error" $
+            liftA3 JsonRpcErr <$> (.: "code") <*> (.: "message") <*> (.:? "data")
+
+
+instance (ToJSON e, ToJSON r) => ToJSON (Response e r) where
+    toJSON (Result ix r) =
+        object [ "jsonrpc" .= ("2.0" :: String)
+               , "result" .= r
+               , "id" .= ix
+               ]
+
+    toJSON (Errors (JsonRpcErr c msg err)) =
+        object [ "jsonrpc" .= ("2.0" :: String)
+               , "id" .= Null
+               , "error" .=
+                    object [ "code" .= c
+                           , "message" .= msg
+                           , "data" .= err
+                           ]
+               ]
 
 
 -- | This is the type used to specify JSON-RPC endpoints
@@ -75,12 +107,26 @@ instance (RunClient m, KnownSymbol method, ToJSON p, FromJSON e, FromJSON r)
     => HasClient m (JsonRpc method p e r) where
 
     type Client m (JsonRpc method p e r)
-        = Word64 -> p -> m (Either String r)
+        = Word64 -> p -> m (Either (JsonRpcErr e) r)
 
     clientWithRoute _ _ req ix p =
         let client = clientWithRoute (Proxy @m) (Proxy @(JsonRpcEndpoint p e r))
-            repack (Result _ r)     = Right r
-            repack (Errors _ msg _) = Left msg
+            repack (Result _ r) = Right r
+            repack (Errors x)   = Left x
         in repack <$> client req (Request (symbolVal $ Proxy @method) p ix)
 
     hoistClientMonad _ _ f x ix p = f $ x ix p
+
+
+instance (KnownSymbol method, FromJSON p, ToJSON e, ToJSON r)
+    => HasServer (JsonRpc method p e r) context where
+
+    type ServerT (JsonRpc method p e r) m = p -> m (Either (JsonRpcErr e) r)
+
+    route _ cx = route (Proxy @(JsonRpcEndpoint p e r)) cx . fmap f
+        where
+        f x (Request _ p ix) = g ix <$> x p
+        g ix (Right r) = Result ix r
+        g _ (Left e)   = Errors e
+
+    hoistServerWithContext _ _ f x p = f $ x p
